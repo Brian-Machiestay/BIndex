c7cbe786aa77a7af12de2835d9f485f9291f0793
Brian-Machiestay <brianmachiestay@gmail.com>
2023-05-23 19:56:24 +0000

    use branching and setup react folder folder

--- client-side/src/App.js
+++ client-side/src/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import './App.css';
+import Header from './components/header.jsx';
 
 function App() {
     return (
-	<p>Hello World!</p>
+	<Header><Header/>
   );
 }
 
---
+++ client-side/src/components/header.jsx
@@ -0,0 +1,11 @@
+// component to render the header section of the application
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+function Header() {
+    return (
+	<p>Food!</p>
+    );
+}
+
+export default Header;
