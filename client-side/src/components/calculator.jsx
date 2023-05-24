3b8a1feeb824a495142203523b010ad51b845315
Brian-Machiestay <brianmachiestay@gmail.com>
2023-05-24 05:01:10 +0000

    create metricInput component

--- client-side/src/components/calculator.jsx
+++ client-side/src/components/calculator.jsx
@@ -4,6 +4,12 @@ function Calc () {
     return (
         <div className="bmi-div">
             <h2>Enter your details below</h2>
+            <div className="metric-options">
+                <input type="radio" id="metric" name="metric-options" value="metric" />
+                <label for="Metric">Metric</label><br />
+                <input type="radio" id="imperial" name="metric-options" value="imperial" />
+                <label for="imperial">Imperial</label><br />
+            </div>
         </div>
     )
 }
